0952e053ed537c8422ad8778be4551e9131cbf69
jhalfpennycodes <jhalfpennycodes@gmail.com>
2025-09-17 15:46:30 +0100

    Debug logs add more

--- server/server.js
+++ server/server.js
@@ -1099,19 +1099,23 @@ app.post("/", async (req, res) => {
     selectedCountry = countries.find(
       (country) => country.alpha2 === alpha2Code
     );
+    console.log("Selected country: ", selectedCountry);
+    console.log("Country alpha 3 code: ", selectedCountry.alpha3);
     const response = await axios.get(API_URL + selectedCountry.alpha3);
     const continentResponse = await axios.get(
       CONT_URL + selectedCountry.continent
     );
     const data = response.data;
     const continentData = continentResponse.data;
-
+    conso;
+    console.log("Response data: ", data);
+    console.log("Continent response data: ", continentData);
     const worldPercentages = calculateWorldPercentage(data);
     const continentPercentages = calculateContinentPercentage(
       data,
       continentData
     );
-
+    console.log("DATA to send: ", sendData);
     const sendData = {
       country: selectedCountry.name,
       rank: data[0].rank,
